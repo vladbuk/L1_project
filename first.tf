677fa99cf2dd79f95b4ede2e34bb98f7be0b1aef
vladbuk <vladbuk@gmail.com>
2022-10-29 21:18:04 +0300

    allow ssh

--- first.tf
+++ first.tf
@@ -60,3 +60,20 @@ resource "aws_security_group" "allow_http" {
     Name = "allow_http_https"
   }
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"  
+
+  ingress {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
