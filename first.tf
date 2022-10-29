37cc31c4663e92c6d947130dcecb551536209525
vladbuk <vladbuk@gmail.com>
2022-10-29 19:33:03 +0300

    security group + apache

--- first.tf
+++ first.tf
@@ -4,10 +4,19 @@ resource "aws_instance" "t2micro_ubuntu22" {
     count = 1
     ami = "ami-0caef02b518350c8b"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [ aws_security_group.allow_http.id ]
     tags = {
         Name = "t2micro_ubuntu22"
         Env = "test"
     }
+    user_data = <<EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install -y apache2
+    myip=$(curl ifconfig.io)
+    echo "<h1>My webserver running on IP: $myip</h1><p>builded by Terraform</p>" | sudo tee /var/www/html/index.html
+    sudo systemctl enable apache2.service --now
+EOF
 }
 
 resource "aws_security_group" "allow_http" {
