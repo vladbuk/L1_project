ef9006c544e89810e090dd578c113ab36cbb3c78
vladbuk <vladbuk@gmail.com>
2022-10-29 21:56:54 +0300

    mc again

--- first.tf
+++ first.tf
@@ -13,12 +13,11 @@ resource "aws_instance" "t2micro_ubuntu22" {
     user_data = <<EOF
 #!/bin/bash
 sudo apt update
-sudo apt install -y apache2
+sudo apt install -y apache2 mc
 myip=$(curl ifconfig.io)
 echo "<h1>My webserver running on IP: $myip</h1><p>builded by Terraform</p>" | sudo tee /var/www/html/index.html
 sudo systemctl enable apache2.service
 sudo systemctl start apache2.service
-sudo apt install mc
 EOF
 }
 
