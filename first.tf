bf154375ee200f14e1f8c0c617981817e3340560
vladbuk <vladbuk@gmail.com>
2022-10-29 19:05:32 +0300

    security group

--- first.tf
+++ first.tf
@@ -9,3 +9,26 @@ resource "aws_instance" "t2micro_ubuntu22" {
         Env = "test"
     }
 }
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"  
+  ingress {
+    description      = "http from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
