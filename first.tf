7830d2ea8dd0afb7355cecada6490a027f89d143
vladbuk <vladbuk@gmail.com>
2022-10-29 19:08:26 +0300

    tags

--- first.tf
+++ first.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "t2micro_ubuntu22" {
     count = 1
     ami = "ami-0caef02b518350c8b"
     instance_type = "t2.micro"
-    tags {
+    tags = {
         Name = "t2micro_ubuntu22"
         Env = "test"
     }
