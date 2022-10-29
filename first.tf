8a2e320659fbb5f3e1944ffb1e6204a174712946
vladbuk <vladbuk@gmail.com>
2022-10-29 21:31:29 +0300

    install mc

--- first.tf
+++ first.tf
@@ -18,6 +18,7 @@ myip=$(curl ifconfig.io)
 echo "<h1>My webserver running on IP: $myip</h1><p>builded by Terraform</p>" | sudo tee /var/www/html/index.html
 sudo systemctl enable apache2.service
 sudo systemctl start apache2.service
+sudo apt install mc
 EOF
 }
 
