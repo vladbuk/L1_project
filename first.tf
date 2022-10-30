c5c6c94dd9817612c40644362edaea49845bd726
vladbuk <vladbuk@gmail.com>
2022-10-30 09:19:24 +0200

    external file

--- first.tf
+++ first.tf
@@ -10,15 +10,7 @@ resource "aws_instance" "t2micro_ubuntu22" {
         Name = "t2micro_ubuntu22"
         Env = "test"
     }
-    user_data = <<EOF
-#!/bin/bash
-sudo apt update
-sudo apt install -y apache2 mc
-myip=$(curl ifconfig.io)
-echo "<h1>My webserver running on IP: $myip</h1><p>builded by Terraform</p>" | sudo tee /var/www/html/index.html
-sudo systemctl enable apache2.service
-sudo systemctl start apache2.service
-EOF
+    user_data = file("user_data.sh")
 }
 
 resource "aws_security_group" "allow_http" {
