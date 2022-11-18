fa1b68799705e93a212835d8d3f351d3d4b9a183
vladbuk <vladbuk@gmail.com>
2022-11-18 20:03:08 +0200

    jenkins new ami

--- 04-jenkins-ami/main.tf
+++ 04-jenkins-ami/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {}
 
 resource "aws_instance" "t2micro_jenkins" {
-    ami = "ami-03cfca73e4b3a3ea5"
+    ami = "ami-0e8cbae065d25f022"
     instance_type = "t2.micro"
     vpc_security_group_ids = [ aws_security_group.allow_http.id, aws_security_group.allow_ssh.id ]
     key_name = "ter_aws_key"
