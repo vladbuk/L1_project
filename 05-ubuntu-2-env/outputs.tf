c2ff4924f9d5232a9df787d3f54f08217cdcb745
vladbuk <vladbuk@gmail.com>
2022-11-18 20:26:55 +0200

    outputs

--- 05-ubuntu-2-env/outputs.tf
+++ 05-ubuntu-2-env/outputs.tf
@@ -1,19 +1,19 @@
-output "instance_id" {
+output "instance_test_id" {
   description = "ID of the t2micro_ubuntu_test"
   value       = aws_instance.t2micro_ubuntu_test.id
 }
 
-output "instance_public_ip" {
+output "instance_test_public_ip" {
   description = "Public IP address of the t2micro_ubuntu_test"
   value       = aws_instance.t2micro_ubuntu_test.public_ip
 }
 
-output "instance_id" {
+output "instance_prod_id" {
   description = "ID of the t2micro_ubuntu_prod"
   value       = aws_instance.t2micro_ubuntu_prod.id
 }
 
-output "instance_public_ip" {
+output "instance_prod_public_ip" {
   description = "Public IP address of the t2micro_ubuntu_prod"
   value       = aws_instance.t2micro_ubuntu_prod.public_ip
 }
