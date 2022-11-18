5ed3450a21f063f5e1f2f7976e34f33749879730
vladbuk <vladbuk@gmail.com>
2022-11-18 20:28:14 +0200

    user data

--- 05-ubuntu-2-env/user_data.sh
+++ 05-ubuntu-2-env/user_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt update && \
-sudo apt upgrade -y \
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && \
-sudo apt update && \
-apt-cache policy docker-ce && \
-sleep 5 && \
-sudo apt install -y docker-ce && \
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sleep 5
+sudo apt install -y docker-ce
 sudo systemctl status docker
 sudo usermod -aG docker ubuntu
