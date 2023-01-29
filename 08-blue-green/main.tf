d7a95f69298142778fb60d2b0ca41a50fa1bf715
vladbuk <vladbuk@gmail.com>
2023-01-29 16:38:30 +0200

    detailed network conf

--- 08-blue-green/main.tf
+++ 08-blue-green/main.tf
@@ -16,6 +16,56 @@ data "aws_ami" "ubuntu20_latest" {
 
 data "aws_availability_zones" "working_zones" {}
 
+data "instance_ubuntu_public_ip" {
+  value = aws_instance.ubuntu.public_ip
+}
+
+#-------------------------------------------------#
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "172.16.0.0/16"
+  tags = {
+    "Name" = "main_vpc"
+    "Env" = "test"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "172.16.1.0/24"
+  availability_zone = data.aws_availability_zones.working_zones[0]
+  tags = {
+    "Name" = "Subnet1"
+  }
+}
+
+resource "aws_network_interface" "ether1" {
+  subnet_id = aws_subnet.subnet1.id
+  private_ips = [ "172.16.1.10" ]
+  tags = {
+    "Name" = "primary_net_interface"
+  }
+}
+
+resource "aws_internet_gateway" "main_gw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+#-------------------------------------------------#
+
+resource "aws_instance" "ubuntu" {
+  ami = data.aws_ami.ubuntu20_latest
+  instance_type = "t2.micro"
+  key_name = "ter_aws_key.pem"
+  vpc_security_group_ids = [ aws_security_group.allow_ports.id ]
+  network_interface {
+    network_interface_id = aws_network_interface.ether1.id
+    device_index = "0"
+  }
+}
+
+#-------------------------------------------------#
+
 resource "aws_security_group" "allow_ports" {
   name        = "allow_in_ports"
   description = "Open only needed ports" 
@@ -54,7 +104,9 @@ resource "aws_security_group" "allow_ports" {
 
 
 
-
+output "instance_ubuntu_public_ip" {
+  value = aws_instance.ubuntu.public_ip
+}
 
 output "aws_ami" {
     value = data.aws_ami.ubuntu20_latest.id
