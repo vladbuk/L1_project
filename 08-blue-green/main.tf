5eb117a4e5da6a607ddcf7dc6a38affee422f1f3
vladbuk <vladbuk@gmail.com>
2023-01-29 17:01:13 +0200

    just vpc

--- 08-blue-green/main.tf
+++ 08-blue-green/main.tf
@@ -16,10 +16,6 @@ data "aws_ami" "ubuntu20_latest" {
 
 data "aws_availability_zones" "working_zones" {}
 
-data "instance_ubuntu_public_ip" {
-  value = aws_instance.ubuntu.public_ip
-}
-
 #-------------------------------------------------#
 
 resource "aws_vpc" "main_vpc" {
@@ -33,7 +29,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "subnet1" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = "172.16.1.0/24"
-  availability_zone = data.aws_availability_zones.working_zones[0]
+  availability_zone = data.aws_availability_zones.working_zones.names[0]
   tags = {
     "Name" = "Subnet1"
   }
@@ -42,6 +38,7 @@ resource "aws_subnet" "subnet1" {
 resource "aws_network_interface" "ether1" {
   subnet_id = aws_subnet.subnet1.id
   private_ips = [ "172.16.1.10" ]
+  # security_groups = [ aws_security_group.allow_ports.id ]
   tags = {
     "Name" = "primary_net_interface"
   }
@@ -53,60 +50,63 @@ resource "aws_internet_gateway" "main_gw" {
 
 #-------------------------------------------------#
 
-resource "aws_instance" "ubuntu" {
-  ami = data.aws_ami.ubuntu20_latest
-  instance_type = "t2.micro"
-  key_name = "ter_aws_key.pem"
-  vpc_security_group_ids = [ aws_security_group.allow_ports.id ]
-  network_interface {
-    network_interface_id = aws_network_interface.ether1.id
-    device_index = "0"
-  }
-}
+# resource "aws_instance" "ubuntu" {
+#   ami = data.aws_ami.ubuntu20_latest.id
+#   instance_type = "t2.micro"
+#   key_name = "ter_aws_key.pem"
+#   # vpc_security_group_ids = [ aws_security_group.allow_ports.id ]
+#   network_interface {
+#     network_interface_id = aws_network_interface.ether1.id
+#     device_index = "0"
+#   }
+#   tags = {
+#     Name = "ubuntu"
+#   }
+# }
 
 #-------------------------------------------------#
 
-resource "aws_security_group" "allow_ports" {
-  name        = "allow_in_ports"
-  description = "Open only needed ports" 
+# resource "aws_security_group" "allow_ports" {
+#   name        = "allow_in_ports"
+#   description = "Open only needed ports" 
 
-  dynamic "ingress" {
-    for_each = ["22", "80", "8080"]
-    content {
-      description      = "open tcp ports"
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  }
+#   dynamic "ingress" {
+#     for_each = ["22", "80", "8080"]
+#     content {
+#       description      = "open tcp ports"
+#       from_port        = ingress.value
+#       to_port          = ingress.value
+#       protocol         = "tcp"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#     }
+#   }
 
-  ingress {
-    description      = "open icmp traffic"
-    from_port        = -1
-    to_port          = -1
-    protocol         = "icmp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description      = "open icmp traffic"
+#     from_port        = -1
+#     to_port          = -1
+#     protocol         = "icmp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    description = "Outbound all packets"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   egress {
+#     description = "Outbound all packets"
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "allow_in_ports"
-  }
-}
+#   tags = {
+#     Name = "allow_in_ports"
+#   }
+# }
 
+#----------------------------------------#
 
-
-output "instance_ubuntu_public_ip" {
-  value = aws_instance.ubuntu.public_ip
-}
+# output "instance_ubuntu_public_ip" {
+#   value = aws_instance.ubuntu.public_ip
+# }
 
 output "aws_ami" {
     value = data.aws_ami.ubuntu20_latest.id
